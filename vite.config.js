da9ee5576c12790a4d62788943bbb359033ef350
Unik Yadav <132219783+UnikYadav1083@users.noreply.github.com>
2024-12-25 14:16:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This will make your app accessible on any device in the local network.
+    port: 3000         // Change to any port you prefer, e.g., 3000.
+  }
 })
